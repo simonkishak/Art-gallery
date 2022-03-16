d3ec09d00c62a60893bf38e8880a3bc0be9d59c1
Kishore <kishore07kzr@gmail.com>
2022-03-16 15:13:23 +0530

    made some improvements and added functionality

--- src/contract/artgallery.sol
+++ src/contract/artgallery.sol
@@ -16,8 +16,11 @@ interface IERC20Token {
 
 contract  ArtGallery {
 
+    //Variable used as the index to store all artworks
     uint internal artworksLength = 0;
-    address internal cUsdTokenAddress = 0x874069Fa1Eb16D44d622F2e0Ca25eeA172369bC1;
+
+    //Address of the cUSD erc-20 token
+    address internal constant cUsdTokenAddress = 0x874069Fa1Eb16D44d622F2e0Ca25eeA172369bC1;
 
     struct  Artwork {
         address payable owner;
@@ -27,34 +30,47 @@ contract  ArtGallery {
         uint price;
         uint likes;
         uint disLikes;
-         
+        mapping(address => bool) hasLiked;
+        mapping(address => bool) hasDisliked;
+        bool forSale;  
     }
 
+    //Event that will emit when a new art is uploaded
+    event newArt(address indexed owner, uint index);
+
+    //Even that will emit when an art is bought
+    event artBought(address indexed seller, uint index, uint price, address indexed buyer);
+
+    //Mapping that assigns index to all artworks
     mapping (uint =>  Artwork) internal artworks;
 
+    //Modifier that allows only the owner of the art to access the functions
+    modifier onlyOwner(uint _index){
+        require(msg.sender == artworks[_index].owner, "Only the owner can access this function");
+        _;
+    }
 
+    //Function used to add an artwork 
     function  addArtwork(
         string memory _image,
         string memory _description, 
         uint _size, 
         uint _price
     ) public {
-        uint _likes =0;
-        uint _disLikes =0;
-         artworks [artworksLength] =  Artwork(
-            payable(msg.sender),
-            
-            _image,
-            _description,
-            _size,
-            _price,
-            _likes,
-            _disLikes
-             
-        );
+        Artwork storage art = artworks[artworksLength];
+
+        art.owner = payable(msg.sender);
+        art.image = _image;
+        art.description = _description;
+        art.size = _size;
+        art.price = _price;
+        art.forSale= true;
+
+        emit newArt(msg.sender, artworksLength);
         artworksLength++;
     }
 
+    //Returns all the artworks by index
     function getArtwork(uint _index) public view returns (
         address payable,
         string memory, 
@@ -76,42 +92,39 @@ contract  ArtGallery {
         );
     }
 
-    function Like (uint _index)public{
-        artworks[_index].likes ++;
+    //Function that is used to like the art ensuring that user has not already liked
+    function Like(uint _index)public{
+        require(artworks[_index].hasLiked[msg.sender] == false, "User an like the art only once");
+        artworks[_index].likes++;
+        artworks[_index].hasLiked[msg.sender] = true;
     } 
     
+    //Function that is used to dislike the art ensuring that the  user has not  already disliked 
     function Dislike(uint _index) public{
-        artworks[_index].disLikes ++;
+        require(artworks[_index].hasDisliked[msg.sender] == false, "User an dislike the art only once");
+        artworks[_index].disLikes++;
+        artworks[_index].hasDisliked[msg.sender] = true;
     }
 
-    function Delete (uint _index)public{
-        require(artworks[_index].owner==msg.sender, "Error");
+    //Function to Delete the art work
+    function Delete(uint _index)public onlyOwner(_index){
         delete(artworks[_index]);
     }
 
+
+    //Function using which the owner can change the price of the art making it viable for reselling
      function editArtwork(
         uint256 _index,
-        string memory _image,
-        string memory _description ,
-        uint _size,
         uint _price
-    ) public {
-        require(msg.sender == artworks[_index].owner, "Error");// this is to ensure that only the owner of the artwork can edit the artwork
-        uint _disLikes = artworks[_index].disLikes;
-        uint _likes = artworks[_index].likes;
-        artworks[_index] = Artwork(
-            payable(msg.sender),
-            _image,
-            _description,
-            _size,
-            _price,
-            _likes,
-            _disLikes
-        );
+    ) public onlyOwner(_index){
+        artworks[_index].price = _price;
     }
 
 
+
+    //Function which a use can use to buy the art and be the owner of that art
     function buyArtwork(uint _index) public payable  {
+        require(artworks[_index].forSale == true);
         require(
           IERC20Token(cUsdTokenAddress).transferFrom(
             msg.sender,
@@ -124,9 +137,15 @@ contract  ArtGallery {
         artworks[_index].owner = payable(msg.sender);
          
     }
+
+    //Function which the owner can use to stop people from buying the artwork
+    function closeSale(uint _index) public onlyOwner(_index){
+        artworks[_index].forSale = false;
+    }
     
+
+    //Function that returns the total number of art works 
     function getartworksLength() public view returns (uint) {
         return (artworksLength);
     }
 }
-
