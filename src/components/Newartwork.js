1b2ab884a39738ecf57dc242d35908b42db0f1b6
Daniel Ogbuti <danielogbuti@gmail.com>
2022-03-30 12:23:20 +0100

    edited frontend

--- src/components/Newartwork.js
+++ src/components/Newartwork.js
@@ -1,76 +1,80 @@
-import React from 'react'
+import React from "react";
 
 import { useState } from "react";
 
 const NewArtwork = (props) => {
+  const [url, setUrl] = useState("");
+  const [description, setDescription] = useState("");
+  const [size, setSize] = useState("");
+  const [price, setPrice] = useState(0);
 
+  const submitHandler = (e) => {
+    e.preventDefault();
 
- const [url, setUrl] = useState('');
- const [description, setDescription] = useState('');
- const [size, setSize] = useState('');
- const [price, setPrice] = useState(0);
- 
- const submitHandler = (e) => {
-  e.preventDefault();
-
-  if(!url || !description || !size || !price) {
-    alert('Please fill in all the required fields')
-    return
-  }
-  props.addArtwork(url, description, size, price);
-
-  setUrl('')
-  setDescription('')
-  setSize('')
-  setPrice('')
-   
-};
-
-return (
-    <>
-    <div className="mb-3">
-    <form onSubmit={submitHandler} >
-      <div className="form-group">
-        <input type="text"
-               className="form-control"
-               value={url}
-               onChange={(e) => setUrl(e.target.value)}
-               placeholder="image url" />
-      </div>
-      <div className="form-group">
-        <input type="text"
-               className="form-control"
-               value={description}
-               onChange={(e) => setDescription(e.target.value)}
-               placeholder="description"/>
-      </div>
-    
-    
-      <div className="form-group">
-      
-        <input type="text" 
-               className="form-control"
-               value={size}
-               onChange={(e) => setSize(e.target.value)}
-               placeholder="size" />
-      </div>
-    
-      <div className="form-group">
-    
-        <input type="text"
-               className="form-control"
-               value={price}
-               onChange={(e) => setPrice(e.target.value)}
-               placeholder="price"/>
-      </div>
-      <button type="submit" class="btn btn-primary">Add artwork</button>
-    </form>
-    </div>
-      </>      
-               
-        )
+    if (!url || !description || !size || !price) {
+      alert("Please fill in all the required fields");
+      return;
     }
-    export default NewArtwork;
+    props.addArtwork(url, description, size, price);
 
-  
- 
+    setUrl("");
+    setDescription("");
+    setSize("");
+    setPrice("");
+  };
+
+  return (
+    <>
+      <div className="mb-3">
+        <form onSubmit={submitHandler}>
+          <div className="form-group">
+            <input
+              type="text"
+              className="form-control"
+              value={url}
+              onChange={(e) => setUrl(e.target.value)}
+              placeholder="image url"
+              required
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="text"
+              className="form-control"
+              value={description}
+              onChange={(e) => setDescription(e.target.value)}
+              placeholder="description"
+              required
+            />
+          </div>
+
+          <div className="form-group">
+            <input
+              type="text"
+              className="form-control"
+              value={size}
+              onChange={(e) => setSize(e.target.value)}
+              placeholder="size"
+              required
+            />
+          </div>
+
+          <div className="form-group">
+            <input
+              type="text"
+              className="form-control"
+              value={price}
+              onChange={(e) => setPrice(e.target.value)}
+              placeholder="price"
+              required
+            />
+          </div>
+          <button type="submit" class="btn btn-primary">
+            Add artwork
+          </button>
+        </form>
+      </div>
+    </>
+  );
+};
+export default NewArtwork;
